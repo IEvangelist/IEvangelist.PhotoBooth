05150f2d3d8be3cab85d7425d03a52ca3d5d89a3
David Pine <david.pine.7@gmail.com>
2018-07-02 20:22:57 -0500

    Add meta-tags for share page

--- IEvangelist.PhotoBooth/ClientApp/src/app/shared/shared.component.ts
+++ IEvangelist.PhotoBooth/ClientApp/src/app/shared/shared.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ImageService } from '../services/image.service';
 
@@ -15,7 +16,8 @@ export class SharedComponent implements OnInit {
 
     constructor(
         private readonly imageService: ImageService,
-        private readonly route: ActivatedRoute) {
+        private readonly route: ActivatedRoute,
+        private readonly meta: Meta) {
         this.route.params.subscribe(params => this.id = params['id']);
     }
 
@@ -24,6 +26,19 @@ export class SharedComponent implements OnInit {
             const shareUrl = await this.imageService.get(this.id);
             if (shareUrl) {
                 this.url = shareUrl.url;
+
+                // TODO: add more meta tags here
+                // facebook, instagram, foursquare, linkedin, etc.
+                this.meta.addTags([
+                    { name: 'twitter:card', content: 'summary_large_image' },
+                    { name: 'twitter:site', content: 'www.creamcitycode.com' },
+                    { name: 'twitter:creator', content: 'David Pine & Ben Felda' },
+                    { property: 'og:url', content: location.href },
+                    { property: 'og:title', content: 'Cream City Code -- 2018' },
+                    { property: 'og:description', content: 'Pictures from the Cream City Code 2018 #DeveloperCommunity Photo Booth App.' },
+                    { property: 'og:image', content: this.url }
+                ],
+                    true);
             }
         } catch (e) {
             console.error(e);
