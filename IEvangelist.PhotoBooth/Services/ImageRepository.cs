f6a64c1aa52545e6556b18f8bacae0048ef194f6
David Pine <david.pine.7@gmail.com>
2018-07-02 20:46:48 -0500

    Added memory caching of azure storage fetch

--- IEvangelist.PhotoBooth/Services/ImageRepository.cs
+++ IEvangelist.PhotoBooth/Services/ImageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using IEvangelist.PhotoBooth.Providers;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
@@ -9,21 +10,32 @@ namespace IEvangelist.PhotoBooth.Services
     public class ImageRepository : IImageRepository
     {
         private readonly IContainerProvider _containerProvider;
+        private readonly IMemoryCache _cache;
         private readonly ILogger<ImageRepository> _logger;
 
         public ImageRepository(
             IContainerProvider containerProvider,
+            IMemoryCache cache,
             ILogger<ImageRepository> logger)
         {
             _containerProvider = containerProvider ?? throw new ArgumentNullException(nameof(containerProvider));
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<Uri> GetImageUriAsync(string id)
         {
-            var container = await _containerProvider.GetContainerAsync();
-            var reference = await container.GetBlobReferenceFromServerAsync(id);
-            return reference.Uri;
+            var cachedUri = await _cache.GetOrCreateAsync(id, async entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromHours(6);
+
+                var container = await _containerProvider.GetContainerAsync();
+                var reference = await container.GetBlobReferenceFromServerAsync(id);
+
+                return reference.Uri;
+            });
+
+            return cachedUri;
         }
 
         public async Task UploadImageAsync(string id, string filePath)
--- IEvangelist.PhotoBooth/Startup.cs
+++ IEvangelist.PhotoBooth/Startup.cs
@@ -21,6 +21,7 @@ namespace IEvangelist.PhotoBooth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen(options =>
             {
